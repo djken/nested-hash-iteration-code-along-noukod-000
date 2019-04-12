19e2933a96b625bff61666e28483ac81f0644e5c
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-12 13:28:16 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,13 +14,22 @@ require 'pry'
     }
   }
   
+# def remove_strawberry(contacts)
+#   contacts.each do |name, name_value|
+#     name_value.each do |attribute, data|
+#       if attribute == :favorite_icecream_flavors
+#         data.delete_if do |ice_cream| 
+#           ice_cream == "strawberry"
+#       end
+#     end
+#   end
+# end
 def remove_strawberry(contacts)
-  contacts.each do |name, name_value|
-    name_value.each do |attribute, data|
+  contacts.each do |person, contact_details_hash|
+    contact_details_hash.each do |attribute, data|
       if attribute == :favorite_icecream_flavors
-        data.delete_if do |ice_cream| 
-          ice_cream == "strawberry"
+        data.delete_if { |ice_cream| ice_cream == "strawberry"}
       end
     end
   end
-end
+end	end
