59ca33ee0db90a09e0b030d318f6ca78ccd45878
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-12 13:28:44 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -24,6 +24,7 @@ require 'pry'
 #     end
 #   end
 # end
+
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     contact_details_hash.each do |attribute, data|
@@ -32,4 +33,5 @@ def remove_strawberry(contacts)
       end
     end
   end
-end	end
+end	
+end
